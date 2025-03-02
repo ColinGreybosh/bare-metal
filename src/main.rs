b57f6b1a1d61d0f7b64ca9e7a1cacdcb02254a81
Colin Greybosh <colingreybosh@gmail.com>
2025-03-02 15:20:02 -0800

    Buildable program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn handle_panic(_panic_info: &PanicInfo) -> ! {
+    loop {}
 }
